3591db46843c2ba3e14de248d519b6f4c9985434
Ann <ann.suparada@gmail.com>
2023-11-26 21:17:01 -0800

    create smart contract with sepolia network

---
+++ smart_contract/hardhat.config.js
@@ -0,0 +1,13 @@
+require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.0",
+  networks: {
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.API_KEY}`,
+      accounts: [process.env.PRIVATE_KEY],
+    }
+  }
+};
