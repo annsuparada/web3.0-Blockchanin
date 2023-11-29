bee5d04f9181207ad93c6b8373fe8ec054f4a7a6
Ann <ann.suparada@gmail.com>
2023-11-29 15:05:39 -0800

    update transaction container

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -59,7 +59,10 @@ const Transactions = () => {
 
   return (
     <div className="flex w-full justify-center items-center 2xl:px-20 gradient-bg-transactions">
-      <div className="flex flex-col md:p-12 py-12 px-4" id="transactions">
+      <div
+        className="flex flex-col max-w-screen-2xl	 md:p-12 py-12 px-4"
+        id="transactions"
+      >
         {currentAccount ? (
           <h3 className="text-white text-3xl text-center my-2">
             Latest Transactions
